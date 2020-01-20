275618d3ecc22c523333a01834f09ebd6c858b20
Lee McCormick <lzm0010@gmail.com>
2020-01-20 16:27:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,9 +19,9 @@ def monopoly_with_third_tier
         four_pieces_owned: 200
       },
       :names => {
-        shortline_railroad: 25,
-        b_and_o_railroad: 25,
-        pennsylvania_railroad: 25,
+        shortline_railroad: {},
+        b_and_o_railroad: {},
+        pennsylvania_railroad: {},
         reading_railroad: {}
       }
     }
