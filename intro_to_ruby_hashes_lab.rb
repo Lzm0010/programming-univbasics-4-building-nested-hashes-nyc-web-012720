c28bf3656d0b896ba888f9948a36884d90839f02
Lee McCormick <lzm0010@gmail.com>
2020-01-20 16:30:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def monopoly_with_fourth_tier
         four_pieces_owned: 200
       },
       :names => {
-        shortline_railroad: {"mortgage_value" => "$100"},
+        shortline_railroad: {"mortgage_value" => "$800"},
         b_and_o_railroad: {"mortgage_value" => "$400"},
         pennsylvania_railroad: {"mortgage_value" => "$200"},
         reading_railroad: {"mortgage_value" => "$100"}
